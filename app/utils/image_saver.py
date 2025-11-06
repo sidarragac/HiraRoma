ffe31b1d95b507c3e9ca8d5c221eee92ff908618
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-11-06 15:18:22 -0500

    Added timestamop to folder creation

--- app/utils/image_saver.py
+++ app/utils/image_saver.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from flask import current_app
 from werkzeug.utils import secure_filename
@@ -7,7 +8,8 @@ def save_image(image):
     upload_folder = current_app.config['UPLOAD_FOLDER']
     
     filename = secure_filename(image.filename)
-    img_path = os.path.join(upload_folder, filename)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    img_path = os.path.join(upload_folder, timestamp, filename)
     
     image.save(img_path)
 
