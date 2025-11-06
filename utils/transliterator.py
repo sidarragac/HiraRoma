d678f58105390d00dcb856f82df211893fff6bee
Santiago Idarraga Ceballos <san200526@gmail.com>
2025-11-06 01:16:14 -0500

    feat: transliterator module to format the text

---
+++ utils/transliterator.py
@@ -0,0 +1,96 @@
+class Transliterator:
+    def __init__(self):
+        self.doubles = {
+            'aa': 'a',
+            'ii': 'i',
+            'uu': 'u',
+            'ee': 'e',
+            'oo': 'o',
+            'nn': 'n'
+        }
+        self.equivalences = {
+            'a': 'あ',
+            'i': 'い',
+            'u': 'う',
+            'e': 'え',
+            'o': 'お',
+
+            # K
+            'ka': 'か',
+            'ki': 'き',
+            'ku': 'く',
+            'ke': 'け',
+            'ko': 'こ',
+
+            # S
+            'sa': 'さ',
+            'shi': 'し',
+            'su': 'す',
+            'se': 'せ',
+            'so': 'そ',
+
+            # T
+            'ta': 'た',
+            'chi': 'ち',
+            'tsu': 'つ',
+            'te': 'て',
+            'to': 'と',
+
+            # N
+            'na': 'な',
+            'ni': 'に',
+            'nu': 'ぬ',
+            'ne': 'ね',
+            'no': 'の',
+
+            # H
+            'ha': 'は',
+            'hi': 'ひ',
+            'fu': 'ふ',
+            'he': 'へ',
+            'ho': 'ほ',
+
+            # M
+            'ma': 'ま',
+            'mi': 'み',
+            'mu': 'む',
+            'me': 'め',
+            'mo': 'も',
+
+            # Y
+            'ya': 'や',
+            'yu': 'ゆ',
+            'yo': 'よ',
+
+            # R
+            'ra': 'ら',
+            'ri': 'り',
+            'ru': 'る',
+            're': 'れ',
+            'ro': 'ろ',
+
+            # W
+            'wa': 'わ',
+            'wo': 'を',
+
+            # N
+            'n': 'ん',
+        }
+
+    def transliterate(self, char):
+        roma = self.equivalences.get(char)
+        if roma:
+            return roma
+        
+        single_char = self.doubles.get(char, '') # Default '', but will never reach that condition.
+        roma = self.equivalences.get(single_char)
+
+        return roma
+
+    def transliterate_text(self, text):
+        roma_text = []
+        for char in text:
+            roma = self.transliterate(char)
+            roma_text.append(roma)
+
+        return roma_text
